f8cc97621406981a5c9bcdb898d1ce23c6f84976
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2024-08-07 20:48:13 +0500

    update code

--- components/StairTransition.jsx
+++ components/StairTransition.jsx
@@ -1,22 +1,20 @@
-"use client"
+"use client";
 
-import { AnimatePresence } from "framer-motion"
-import { usePathname } from "next/navigation"
-import Stairs from "./ui/Stairs"
+import { AnimatePresence } from "framer-motion";
+import { usePathname } from "next/navigation";
+import Stairs from "./Stairs";
 
 const StairTransition = () => {
-    const pathname = usePathname()
-    return (
-        <>
-            <AnimatePresence mode="wait">
-                <div key={pathname}>
-                    <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex">
-                        <Stairs />
-                    </div>
-                </div>
-            </AnimatePresence>
-        </>
-    )
-}
+  const pathname = usePathname();
+  return (
+    <AnimatePresence mode="wait">
+      <div key={pathname}>
+        <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex">
+          <Stairs />
+        </div>
+      </div>
+    </AnimatePresence>
+  );
+};
 
-export default StairTransition
+export default StairTransition;

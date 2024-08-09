04c700b6073248833e934c21c372d1699e5084f0
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2024-08-09 14:05:34 +0500

    update code and anamte photo

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-center xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className='text-center xl:text-left'>
+          <div className='text-center xl:text-left order-2 xl:order-none'>
             <span className='text-xl '>Software Developer</span>
             <h1 className='h1 mb-6 mt-5'>Hello I'm <span className='text-accent-DEAFULT'>Luka Coleman</span>
             </h1>
@@ -24,18 +24,18 @@ const Home = () => {
                 <FiDownload />
               </Button>
               <div className="mb-8 xl:mb-0">
-                <Social  containerStyles="flex gap-6 mt-5" iconStyles="w-9 h-9 border border-accent-DEAFULT rounded-full flex justify-center items-center text-accent-DEAFULT text-base hover:bg-accent-DEAFULT hover:text-primary hover:transition-all duration-500"/>
+                <Social containerStyles="flex gap-6 mt-5" iconStyles="w-9 h-9 border border-accent-DEAFULT rounded-full flex justify-center items-center text-accent-DEAFULT text-base hover:bg-accent-DEAFULT hover:text-primary hover:transition-all duration-500" />
               </div>
             </div>
           </div>
-           {/* image */}
-           <div className="flex">
-            <Photo/>
+          {/* image */}
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
           </div>
         </div>
 
-         
-        </div>
+
+      </div>
     </section>
   )
 }

de77acdb8f6263ad912b50219f11e2b4f36e93ea
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2024-08-19 12:50:21 +0500

    add fix download all files

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,8 @@
 import Photo from '@/components/Photo';
 import Social from '@/components/Social';
 import Stats from '@/components/Stats';
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import { Button } from '@/components/ui/button';
+import React from 'react';
 import { FiDownload } from "react-icons/fi";
 
 const Home = () => {
@@ -13,17 +13,25 @@ const Home = () => {
           {/* text */}
           <div className='text-center xl:text-left order-2 xl:order-none'>
             <span className='text-xl '>Software Developer</span>
-            <h1 className='h1 mb-6 mt-5'>Hello I'm <span className='text-accent-DEAFULT'>zain</span>
+            <h1 className='h1 mb-6 mt-5'>Hello I'm <span className='text-accent-DEAFULT'>Zain</span>
             </h1>
-            <p className='max-w-[500px] mx-auto xl:mx-0 text-white/80'>I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies</p>
+            <p className='max-w-[500px] mx-auto xl:mx-0 text-white/80'>I excel at crafting elegant digital experiences and am proficient in various programming languages and technologies.</p>
             <div className='flex flex-col xl:flex-row items-center gap-8'>
-              <Button
-                variant="outline"
-                size="lg"
-                className="uppercase items-center gap-2 mt-5">
-                <span >Download CV</span>
-                <FiDownload />
-              </Button>
+              <a
+                href="/assets/zain.pdf"
+                download="Zain_Resume.pdf"
+                className="uppercase items-center gap-2 mt-5"
+              >
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="flex items-center gap-2"
+                >
+                  <span>Download CV</span>
+                  <FiDownload />
+                </Button>
+              </a>
+
               <div className="mb-8 xl:mb-0">
                 <Social containerStyles="flex gap-6 mt-5" iconStyles="w-9 h-9 border border-accent-DEAFULT rounded-full flex justify-center items-center text-accent-DEAFULT text-base hover:bg-accent-DEAFULT hover:text-primary hover:transition-all duration-500" />
               </div>
@@ -37,9 +45,7 @@ const Home = () => {
       </div>
       <Stats />
     </section>
-  )
-}
+  );
+};
 
-export default Home
-
-
+export default Home;
